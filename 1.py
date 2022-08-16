effca747a284ec28eae47f0e0ede493fd8e4a4d9
Yarin Zaddik <yarinzaddik@gmail.com>
2022-08-16 19:07:48 +0300

    intial commit

---
+++ 1.py
@@ -0,0 +1,25 @@
+print("hello world!")
+
+a = 4
+print(a)
+b = "Hello 3006"
+print(b)
+
+c = [1, 2, 3]
+c[1] = "aviel"
+print(c[2])
+
+d = False
+
+e = (1, "moshe", False)
+
+f = ["Yarin", "Zaddik", 23, False]
+
+g = {"fname": "aviel", "lname": "buskila", "age": 32, "isMarried": True}
+
+print(g["lname"])
+
+h = [{"name": "seva", "age": 25}, {"name": "maor", "age": 38}]
+
+print(h[0]["name"])
+print(h[1]["name"])
